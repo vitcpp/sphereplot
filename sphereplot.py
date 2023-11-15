a800f760bb19481d7aa3bf907462f03854b406a1
Vitaly Davydov <vitprof@gmail.com>
2023-11-15 09:08:30 +0300

    Change application interface

--- sphereplot.py
+++ sphereplot.py
@@ -47,12 +47,7 @@ class GreatCircleArc:
 		self.pts = [beg, end]
 
 	def __getitem__(self, idx):
-		if idx == 0:
-			return self.pts[0]
-		elif idx == 1:
-			return self.pts[1]
-		assert False
-		return None
+		return self.pts[idx]
 
 class Meridian:
 
@@ -66,33 +61,39 @@ class Parallel:
 
 class Scene:
 
-	def __init__(self):
-		self.objects = []
-
-	def add(self, gobj):
-		self.objects.append(gobj)
-
-	def plot(self):
-		fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(20,20))
+	def __init__(self, fig, rect):
 		self.fig = fig
-		self.axes = ax
-		ax.grid(False)
-		ax.set_xlim(-1, 1)
-		ax.set_ylim(-1, 1)
-		ax.set_zlim(-1, 1)
-		ax.set(xticklabels=[], yticklabels=[], zticklabels=[])
-		ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-		ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-		ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-		ax._axis3don = False
-		ax.set_aspect("equal");
-		ax.view_init(elev=45., azim=15)
+		self.axes = self.fig.add_axes(projection = "3d", rect = rect)
+		self.axes.grid(False)
+		self.axes.set_xlim(-1, 1)
+		self.axes.set_ylim(-1, 1)
+		self.axes.set_zlim(-1, 1)
+		self.axes.set(xticklabels=[], yticklabels=[], zticklabels=[])
+		self.axes.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+		self.axes.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+		self.axes.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+		self.axes._axis3don = False
+		self.axes.set_aspect("equal");
+		self.axes.view_init(elev=45., azim=15)
 		self.__plot_axes()
-		self.__plot_objects()
 
-	def show(self):
-		self.plot()
-		plt.show()
+	def camera(self, elev, azim):
+		self.axes.view_init(elev = elev, azim = azim)
+
+	def plot(self, gobj):
+		self.__plot_object(gobj)
+
+	def parallel(self, lat, **kwargs):
+		self.__plot_parallel(Parallel(lat), **kwargs)
+
+	def meridian(self, lng, **kwargs):
+		self.__plot_meridian(Meridian(lng), **kwargs)
+
+	def vector(self, lng, lat, **kwargs):
+		self.__plot_vector(Vector(Point(lng, lat)), **kwargs)
+
+	def great_circle_arc(self, lng1, lat1, lng2, lat2, **kwargs):
+		self.__plot_arc(GreatCircleArc(Point(lng1, lat1), Point(lng2, lat2)), **kwargs)
 
 	def __plot_axes(self):
 		val = [1,0,0]
@@ -105,29 +106,25 @@ class Scene:
 			self.axes.plot(x, y, z,'-', linewidth=1)
 			self.axes.text(val[v-0], val[v-1], val[v-2], labels[v], color=colors[v], fontsize=12)
 
-	def __plot_objects(self):
-		for gobj in self.objects:
-			self.__plot_object(gobj)
-
 	def __plot_object(self, gobj):
 		if isinstance(gobj, Meridian):
 			self.__plot_meridian(gobj)
 		elif isinstance(gobj, Parallel):
-			self.__plot_Parallel(gobj)
+			self.__plot_parallel(gobj)
 		elif isinstance(gobj, GreatCircleArc):
 			self.__plot_arc(gobj)
 		elif isinstance(gobj, Vector):
 			self.__plot_vector(gobj)
 
-	def __plot_meridian(self, gobj):
+	def __plot_meridian(self, gobj, **kwargs):
 		lngv = gobj.lng
 		latv = np.linspace(-np.pi/2, np.pi/2, 100)
 		x = np.cos(lngv) * np.cos(latv)
 		y = np.sin(lngv) * np.cos(latv)
 		z = np.sin(latv)
-		self.axes.scatter(x, y, z, s = 0.1, color = "darkgray")
+		self.axes.scatter(x, y, z, **kwargs)
 
-	def __plot_Parallel(self, gobj):
+	def __plot_parallel(self, gobj):
 		lngv = np.linspace(0, 2 * np.pi, 100)
 		latv = gobj.lat
 		x = np.cos(lngv) * np.cos(latv)
@@ -135,7 +132,7 @@ class Scene:
 		z = np.sin(latv)
 		self.axes.scatter(x, y, z, s = 0.1, color = "darkgray")
 
-	def __plot_arc(self, garc):
+	def __plot_arc(self, garc, **kwargs):
 		v1 = Vector(garc[0])
 		v2 = Vector(garc[1])
 		v3 = v1.cross_product(v2)
@@ -145,10 +142,11 @@ class Scene:
 		x = np.cos(t) * v2[0] + np.sin(t) * v4[0]
 		y = np.cos(t) * v2[1] + np.sin(t) * v4[1]
 		z = np.cos(t) * v2[2] + np.sin(t) * v4[2]
-		self.axes.scatter(x, y, z, s = 0.1, color = "red")
+		k = { "color" : "red", "s" : 0.1 } | kwargs
+		self.axes.scatter(x, y, z, **k)
 
-	def __plot_vector(self, gvec : Vector):
+	def __plot_vector(self, gvec : Vector, **kwargs):
 		x = np.linspace(0, gvec[0], 100)
 		y = np.linspace(0, gvec[1], 100)
 		z = np.linspace(0, gvec[2], 100)
-		self.axes.plot3D(x, y, z, color = gvec.color)
+		self.axes.plot3D(x, y, z, **kwargs)
--- test.py
+++ test.py
@@ -1,24 +1,26 @@
 from sphereplot import *
 
-scene = Scene()
+fig = plt.figure(figsize = (5, 5))
 
-scene.add(Parallel(0))
-scene.add(Parallel(1))
-scene.add(Parallel(np.pi/2))
-scene.add(Parallel(-1))
-scene.add(Parallel(-np.pi/2))
+scene = Scene(fig, rect = [0, 0, 1, 1])
+
+scene.parallel(0)
+scene.parallel(1)
+scene.parallel(np.pi/2)
+scene.parallel(-1)
+scene.parallel(-np.pi/2)
 
 for lng in np.linspace(0, 2* np.pi, 16):
-	scene.add(Meridian(lng))
+	scene.meridian(lng, s = 0.1, color = "gray")
 
-# scene.add(GreatCircleArc(Point(0, 1), Point(1, 1)))
+scene.vector(0, 1, color="blue")
+scene.vector(1, 1, color="blue")
 
-scene.add(Vector(Point(0, 1), color="blue"))
-scene.add(Vector(Point(1, 1), color="blue"))
+scene.great_circle_arc(0, 0, 0, 1)
+scene.great_circle_arc(0, 1, 1, 1, color = "yellow")
+scene.great_circle_arc(1, 1, 1, 0)
+scene.great_circle_arc(1, 0, 0, 0)
 
-scene.add(GreatCircleArc(Point(0, 0), Point(0, 1)))
-scene.add(GreatCircleArc(Point(0, 1), Point(1, 1)))
-scene.add(GreatCircleArc(Point(1, 1), Point(1, 0)))
-scene.add(GreatCircleArc(Point(1, 0), Point(0, 0)))
+scene.camera(35, 45)
 
-scene.show()
+plt.show()
